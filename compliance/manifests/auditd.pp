b492cd36d614ff05878171cf6f08ba3d1fccd833
Tim Ski <marshyski@gmail.com>
2015-05-13 10:42:13 -0400

    Update auditd.pp

--- compliance/manifests/auditd.pp
+++ compliance/manifests/auditd.pp
@@ -1,36 +1,36 @@
 class compliance::auditd {
 
-# Install audit package
+# Install audit package and dependencies
   package { 'audit':
     ensure => present,
-    }
+  }
 
-# Ensure audit.rules is current
+# Ensure audit.rules is present
   file { '/etc/audit/audit.rules':
     ensure  => present,
     owner   => root,
     group   => root,
     content => template('compliance/audit.rules'),
     require => Package['audit'],
-    }
+  }
 
-# Ensure auditd.conf is current
+# Ensure auditd.conf is present
   file { '/etc/audit/auditd.conf':
     ensure  => present,
     owner   => root,
     group   => root,
-    mode    => 600,
+    mode    => '0600',
     content => template('compliance/auditd.conf'),
     require => Package['audit'],
-    }
+  }
 
-# Ensure daemon restarts when configuration change has been made
+# Ensure daemon is started and restarts when configuration has changed
   service { 'auditd':
     ensure     => running,
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
-    subscribe  => [Package['audit'], File['/etc/audit/audit.rules'], File['/etc/audit/auditd.conf']],
-    require    => [File['/etc/audit/auditd.conf'], File['/etc/audit/audit.rules']];
-    }
+    subscribe  => [File['/etc/audit/audit.rules'], File['/etc/audit/auditd.conf']],
+    require    => [File['/etc/audit/auditd.conf'], File['/etc/audit/audit.rules']],
+  }
 }
